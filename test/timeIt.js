f5e9b4ce01dac3f4a42c3b0b55a751270c7509ad
cicorias <github@cicoria.com>
2015-09-16 16:30:34 -0400

    update minor changes

--- test/timeIt.js
+++ test/timeIt.js
@@ -1,29 +1,39 @@
 import nconf from 'nconf';
 import assert from 'assert';
+import fs from 'fs';
+import crypto from 'crypto';
 import now from 'performance-now';
 import azure from 'azure-storage';
 import uuid from 'node-uuid';
 
-import * as uploader from '../uploader';
 
 describe('Time of Uploads single threaded', function () {
 	var start, end, totalTime;
-	var configFile = __dirname + '/config.1.json';
+	let configFile = __dirname + '/config.1.json';
 	console.log('using file: ' + configFile);
 
 	var blobService;
 
-	nconf.env()
-		.file({ file: configFile, search: true });
-	var containerName = nconf.get("CONTAINER_NAME");
-	var accountName = nconf.get("STORAGE_NAME");
-	var accountKey = nconf.get("STORAGE_KEY");
+	nconf.env().file({ file: configFile, search: true });
+	
+	let containerName = nconf.get("CONTAINER_NAME");
+	let accountName = nconf.get("STORAGE_NAME");
+	let accountKey = nconf.get("STORAGE_KEY");
 
-			var fileName = 'file100m.tst';
+	let warmupFile = 'filewarmup.tst';
+	let testFile = 'testFile.tst';
+	let aMB = 1024 * 1024;
+	let numberOfMB = 10;
+	let fileSize = numberOfMB * aMB;
+		
 	before(function (done) {
-
-		blobService = azure.createBlobService(accountName, accountKey)
-			.withFilter(new azure.ExponentialRetryPolicyFilter());
+		this.timeout(10000);
+		blobService = azure.createBlobService(accountName, accountKey);
+			//.withFilter(new azure.ExponentialRetryPolicyFilter());
+		
+		generateTempFile(warmupFile, aMB, false);
+		generateTempFile(testFile, fileSize, false);
+		
 		done();
     });
 
@@ -35,91 +45,95 @@ describe('Time of Uploads single threaded', function () {
 	afterEach(function (done) {
 		end = now();
 		totalTime = end - start;
-		console.warn(totalTime);
+		var mbs = fileSize / totalTime / 1000;
+		
+		Math.round
+		
+		console.log('\t\ttotalTime: ' + totalTime.toFixed(2) + ' MBs: ' + mbs.toFixed(2) );
 		done();
 	});
 
-	describe('warm up- 1 thread', function () {
+	describe('warm up- 1 thread - 1MB', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var fileName = 'file1m.tst';
-			var ptc = 1;
+			let blobName = uuid();
+			let fileName = warmupFile;
+			let ptc = 1;
 			callIt(blobService, containerName, blobName, fileName, ptc, done);
 		});
 	});
 
-	describe('100 MB upload - 1 thread', function () {
+	describe( numberOfMB + ' MB upload - 1 thread', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var ptc = 1;
-			callIt(blobService, containerName, blobName, fileName, ptc, done);
+			let blobName = uuid();
+			let ptc = 1;
+			callIt(blobService, containerName, blobName, testFile, ptc, done);
 		});
 	});
 
-	describe('100 MB upload - 2 thread', function () {
+	describe(numberOfMB + ' MB upload - 2 thread', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var ptc = 2;
-			callIt(blobService, containerName, blobName, fileName, ptc, done);
+			let blobName = uuid();
+			let ptc = 2;
+			callIt(blobService, containerName, blobName, testFile, ptc, done);
 		});
 	});
-	
-	describe('100 MB upload - 3 thread', function () {
+
+	describe(numberOfMB + ' MB upload - 3 thread', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var ptc = 3;
-			callIt(blobService, containerName, blobName, fileName, ptc, done);
+			let blobName = uuid();
+			let ptc = 3;
+			callIt(blobService, containerName, blobName, testFile, ptc, done);
 		});
 	});
-	
-	describe('10 MB upload - 4 thread', function () {
+
+	describe(numberOfMB + ' MB upload - 4 thread', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var ptc = 4;
-			callIt(blobService, containerName, blobName, fileName, ptc, done);
+			let blobName = uuid();
+			let ptc = 4;
+			callIt(blobService, containerName, blobName, testFile, ptc, done);
 		});
 	});
 
 
-	describe('10 MB upload - 5 thread', function () {
+	describe(numberOfMB + ' MB upload - 5 thread', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var ptc = 5;
-			callIt(blobService, containerName, blobName, fileName, ptc, done);
+			let blobName = uuid();
+			let ptc = 5;
+			callIt(blobService, containerName, blobName, testFile, ptc, done);
 		});
 	});
-	
-	
-	describe('100 MB upload - 6 thread', function () {
+
+
+	describe(numberOfMB + ' MB upload - 6 thread', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var ptc = 6;
-			callIt(blobService, containerName, blobName, fileName, ptc, done);
+			let blobName = uuid();
+			let ptc = 6;
+			callIt(blobService, containerName, blobName, testFile, ptc, done);
 		});
 	});
-	
-	describe('100 MB upload - 7 thread', function () {
+
+	describe(numberOfMB + ' MB upload - 7 thread', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var ptc = 7;
-			callIt(blobService, containerName, blobName, fileName, ptc, done);
+			let blobName = uuid();
+			let ptc = 7;
+			callIt(blobService, containerName, blobName, testFile, ptc, done);
 		});
 	});
-	
-	describe('100 MB upload - 8 thread', function () {
+
+	describe(numberOfMB + ' MB upload - 8 thread', function () {
 		it('just take some time....', function (done) {
 			this.timeout(300000)
-			var blobName = uuid();
-			var ptc = 8;
-			callIt(blobService, containerName, blobName, fileName, ptc, done);
+			let blobName = uuid();
+			let ptc = 8;
+			callIt(blobService, containerName, blobName, testFile, ptc, done);
 		});
 	});
 });
@@ -133,3 +147,38 @@ function callIt(blobService, containerName, blobName, fileName, ptc, done) {
 		done();
 	});
 }
+
+function generateTempFile (fileName, size, hasEmptyBlock) {
+  var blockSize = 4 * 1024 * 1024;
+  var fileInfo = { name: fileName, contentMD5: '', size: size, content: '' };
+
+  var md5hash = crypto.createHash('md5');
+  var offset = 0;
+  var file = fs.openSync(fileName, 'w');
+  var saveContent= size <= blockSize;
+
+  do {
+    var value = crypto.randomBytes(1);
+    var zero = hasEmptyBlock ? (parseInt(value[0], 10) >= 64) : false;
+    var writeSize = Math.min(blockSize, size);
+    var buffer;
+
+    if (zero) {
+      buffer = new Buffer(writeSize);
+      buffer.fill(0);
+    } else {
+      buffer = crypto.randomBytes(writeSize);
+    }
+      
+    fs.writeSync(file, buffer, 0, buffer.length, offset);
+    size -= buffer.length;
+    offset += buffer.length;
+    md5hash.update(buffer);
+
+    if (saveContent) {
+      fileInfo.content += buffer.toString();
+    }
+  } while(size > 0);
+      
+  fileInfo.contentMD5 = md5hash.digest('base64');
+};
